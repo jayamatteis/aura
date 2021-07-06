ebe5c434adc359ec485b2aa3f05167e108ae120a
jayamatteis <matteisjs@vcu.edu>
2021-07-06 03:16:14 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,9 +3,84 @@
  * I am just passing some of the options in the following example.
  * you can also start the plugin using $('.marquee').marquee(); with defaults
 */
-$('.marquee').marquee({
+$('.marquee1').marquee({
 	//duration in milliseconds of the marquee
 	duration: 15000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('.marquee2').marquee({
+	//duration in milliseconds of the marquee
+	duration: 14000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('.marquee3').marquee({
+	//duration in milliseconds of the marquee
+	duration: 13000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('.marquee4').marquee({
+	//duration in milliseconds of the marquee
+	duration: 11000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('.marquee5').marquee({
+	//duration in milliseconds of the marquee
+	duration: 9000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('.marquee6').marquee({
+	//duration in milliseconds of the marquee
+	duration: 6000,
 	//gap in pixels between the tickers
 	gap: 50,
 	//time in milliseconds before the marquee will start animating
